cd93b6745f5efa9247255321538dbaf772dbbebd
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2017-06-28 01:20:41 +0900

    implement bit-parallel common substring search algorithm
    `common_substr_bitparallel` implements Boyer-Moore-like bit-parallel
    algorithm to find common substring of given length.
    The original algorithm is Backward Shift-Add algorithm for k-LCF problem
    [Hirvola, 2016] (which searches longest common substring with up to k
    errors under the Hamming distance). This algorithm is modified:
    
    * to search only perfect matches (up to 0 errors)
    * to return ASAP if we find a specified length common substring and
    * to share position array with algorithm for Indel distance
    
    Reference: <https://aaltodoc.aalto.fi/bitstream/handle/123456789/21625/master_Hirvola_Tommi_2016.pdf>

--- ffuzzypp/strings/common_substr.hpp
+++ ffuzzypp/strings/common_substr.hpp
@@ -31,6 +31,10 @@
 
 	Copyright (C) 2014 Tsukasa OI <floss_ssdeep@irq.a4lg.com>
 
+	CREDIT OF ADDED PORTIONS (BIT-PARALLEL ALGORITHM)
+
+	Copyright (C) 2017 Tsukasa OI <floss_ssdeep@irq.a4lg.com>
+
 */
 #ifndef FFUZZYPP_STRINGS_COMMON_SUBSTR_HPP
 #define FFUZZYPP_STRINGS_COMMON_SUBSTR_HPP
@@ -39,6 +43,9 @@
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
+
+#include <limits>
+#include <type_traits>
 
 #include "../rolling_hash.hpp"
 #include "position_array.hpp"
@@ -100,6 +107,71 @@ namespace internal
 			return false;
 		}
 	};
+
+	template <
+		size_t SubstrSize,
+		size_t MaxSize = std::numeric_limits<size_t>::max()
+	>
+	class common_substr_bitparallel_impl
+	{
+	private:
+		common_substr_bitparallel_impl(void) = delete;
+		common_substr_bitparallel_impl(const common_substr_bitparallel_impl&) = delete;
+	public:
+		static constexpr const size_t substr_size = SubstrSize;
+		static constexpr const size_t max_size = MaxSize;
+		static_assert(0 < substr_size, "substr_size must be nonzero.");
+		static_assert(0 < max_size, "max_size must be nonzero.");
+	public:
+		template <typename TBitmap, char CMin, char CMax>
+		static bool match(
+			const position_array<TBitmap, char, CMin, CMax>& s1,
+			const char* s2, size_t s2len
+		) noexcept
+		{
+			typedef position_array<TBitmap, char, CMin, CMax> pa_type;
+			#ifdef FFUZZYPP_DEBUG
+			assert(s2);
+			assert(s2len >= substr_size);
+			assert(s2len <= max_size);
+			#endif
+			// position array is too short to have such substrings
+			if (pa_type::max_strlen < substr_size)
+				return false;
+			size_t r = substr_size - 1;
+			while (true)
+			{
+				size_t l = r - (substr_size - 1);
+				// we must reverse s2 because bitmap of s1 is reversed.
+				const char* ch = &s2[s2len - 1 - r];
+				TBitmap D = s1[*ch];
+				while (D)
+				{
+					r--;
+					D = (D << 1) & s1[*++ch];
+					if (r == l && D)
+						return true;
+				}
+				// Boyer-Moore-like skipping
+				if (max_size - 1 <= std::numeric_limits<size_t>::max() - substr_size)
+				{
+					// (max_size - 1) + substr_size <= max(size_t)
+					r += substr_size;
+					if (r >= s2len)
+						break;
+				}
+				else
+				{
+					// (max_size - 1) + substr_size > max(size_t)
+					// which means we cannot safely perform r += substr_size first (very unlikely)
+					if (r >= s2len - substr_size)
+						break;
+					r += substr_size;
+				}
+			}
+			return false;
+		}
+	};
 }
 
 template <size_t MaxSize, size_t SubstrSize>
@@ -135,6 +207,112 @@ public:
 	}
 };
 
+template <typename TBitmap, char CMin, char CMax, size_t MaxSize, size_t SubstrSize>
+class common_substr_bitparallel
+{
+private:
+	common_substr_bitparallel(void) = delete;
+	common_substr_bitparallel(const common_substr_bitparallel&) = delete;
+public:
+	static constexpr const size_t max_size = MaxSize;
+	static constexpr const size_t substr_size = SubstrSize;
+	static_assert(0 < substr_size, "substr_size must be nonzero.");
+	static_assert(substr_size <= max_size, "substring size must not be greater than the maximum size.");
+	static_assert(max_size <= position_array<TBitmap, char, CMin, CMax>::max_strlen,
+		"max_size must not be greater than max_strlen of corresponding position_matrix for efficiency.");
+public:
+	static bool match(
+		const position_array<TBitmap, char, CMin, CMax>& s1,
+		const char* s2, size_t s2len
+	) noexcept
+	{
+		#ifdef FFUZZYPP_DEBUG
+		assert(s2);
+		assert(s2len <= max_size);
+		#endif
+		if (s2len < substr_size)
+			return false;
+		return internal
+			::common_substr_bitparallel_impl<substr_size, max_size>
+			::match(s1, s2, s2len);
+	}
+};
+
+template <typename TBitmap, char CMin, char CMax, size_t MaxSize, size_t SubstrSize>
+class common_substr_bitparallel_wrapper
+{
+private:
+	common_substr_bitparallel_wrapper(void) = delete;
+	common_substr_bitparallel_wrapper(const common_substr_bitparallel_wrapper&) = delete;
+public:
+	static constexpr const size_t max_size = MaxSize;
+	static constexpr const size_t substr_size = SubstrSize;
+	static_assert(0 < substr_size, "substr_size must be nonzero.");
+	static_assert(substr_size <= max_size, "substring size must not be greater than the maximum size.");
+	static_assert(max_size <= position_array<TBitmap, char, CMin, CMax>::max_strlen,
+		"max_size must not be greater than max_strlen of corresponding position_matrix for efficiency.");
+public:
+	static bool match(
+		const char* s1, size_t s1len,
+		const char* s2, size_t s2len
+	) noexcept
+	{
+		#ifdef FFUZZYPP_DEBUG
+		assert(s2);
+		assert(s2len <= max_size);
+		#endif
+		if (s2len < substr_size)
+			return false;
+		position_array<TBitmap, char, CMin, CMax> parray(s1, s1len);
+		return internal
+			::common_substr_bitparallel_impl<substr_size, max_size>
+			::match(parray, s2, s2len);
+	}
+};
+
+
+
+namespace internal
+{
+	template <char CMin, char CMax, size_t MaxSize, size_t SubstrSize, bool IsPositionArrayAvailable>
+	class common_substr_impl_selector;
+
+	template <char CMin, char CMax, size_t MaxSize, size_t SubstrSize>
+	class common_substr_impl_selector<CMin, CMax, MaxSize, SubstrSize, true>
+	{
+	private:
+		common_substr_impl_selector(void) = delete;
+		common_substr_impl_selector(const common_substr_impl_selector&) = delete;
+	public:
+		static bool match(
+			const char* s1, size_t s1len,
+			const char* s2, size_t s2len
+		) noexcept
+		{
+			typename auto_position_array<MaxSize, char, CMin, CMax>::type parray(s1, s1len);
+			return common_substr_bitparallel<typename decltype(parray)::bitmap_type,
+				CMin, CMax, MaxSize, SubstrSize>::match(parray, s2, s2len);
+		}
+	};
+
+	template <char CMin, char CMax, size_t MaxSize, size_t SubstrSize>
+	class common_substr_impl_selector<CMin, CMax, MaxSize, SubstrSize, false>
+	{
+	private:
+		common_substr_impl_selector(void) = delete;
+		common_substr_impl_selector(const common_substr_impl_selector&) = delete;
+	public:
+		static bool match(
+			const char* s1, size_t s1len,
+			const char* s2, size_t s2len
+		) noexcept
+		{
+			return common_substr_hasharray<MaxSize, SubstrSize>::match(s1, s1len, s2, s2len);
+		}
+	};
+}
+
+
 
 template <size_t MaxSize, size_t SubstrSize>
 class common_substr_fast
@@ -146,12 +324,19 @@ public:
 	static constexpr const size_t max_size = MaxSize;
 	static constexpr const size_t substr_size = SubstrSize;
 public:
+	template <
+		char CMin = std::numeric_limits<char>::min(),
+		char CMax = std::numeric_limits<char>::max()
+	>
 	static bool match(
 		const char* s1, size_t s1len,
 		const char* s2, size_t s2len
 	) noexcept
 	{
-		return common_substr_hasharray<max_size, substr_size>::match(s1, s1len, s2, s2len);
+		return internal::common_substr_impl_selector<
+			CMin, CMax, max_size, substr_size,
+			is_auto_position_array_available<MaxSize, char, CMin, CMax>::value
+		>::match(s1, s1len, s2, s2len);
 	}
 };
 
--- tests/cases/small/common_substr.hpp
+++ tests/cases/small/common_substr.hpp
@@ -27,8 +27,11 @@
 #include <cstring>
 
 
-class CommonSubstringTests : public ::testing::Test
+class CommonSubstringTestParams
 {
+private:
+	CommonSubstringTestParams(void) = delete;
+	CommonSubstringTestParams(const CommonSubstringTestParams&) = delete;
 public:
 	static const size_t MaxSize = 64;
 	static const size_t SubstringSizeToTest = 7;
@@ -40,10 +43,9 @@ public:
 		bool has_common_substring;
 	};
 	static const known_entry KnownEntries[];
-
-	typedef strings::common_substr_fast<MaxSize, SubstringSizeToTest> test_type;
 };
-const CommonSubstringTests::known_entry CommonSubstringTests::KnownEntries[] =
+
+const CommonSubstringTestParams::known_entry CommonSubstringTestParams::KnownEntries[] =
 {
 	{"empty", "", "", false},
 	{"identical length 1", "1", "1", false},
@@ -53,50 +55,70 @@ const CommonSubstringTests::known_entry CommonSubstringTests::KnownEntries[] =
 	{"identical length 5", "12345", "12345", false},
 	{"identical length 6", "123456", "123456", false},
 	{"identical length 7", "1234567", "1234567", true},
-	{"identical length 8", "12345678", "12345678", true},
-	{"identical length 9", "123456789", "123456789", true},
-	{"identical length 10", "1234567890", "1234567890", true},
-	{"different length 1", ".", ",", false},
-	{"different length 2", "..", ",,", false},
-	{"different length 3", "...", ",,,", false},
-	{"different length 4", "....", ",,,,", false},
-	{"different length 5", ".....", ",,,,,", false},
-	{"different length 6", "......", ",,,,,,", false},
-	{"different length 7", ".......", ",,,,,,,", false},
-	{"different length 8", "........", ",,,,,,,,", false},
-	{"different length 9", ".........", ",,,,,,,,,", false},
-	{"different length 10", "..........", ",,,,,,,,,,", false},
-	{"mostly identical length 7", "123456.", ",123456", false},
-	{"mostly identical length 8", "1234567.", ",1234567", true},
-	{"mostly identical length 9 A", "1234567..", ",,1234567", true},
-	{"mostly identical length 9 B", ".1234567.", ",1234567,", true},
-	{"mostly identical length 10 A", "1234567...", ",,,1234567", true},
-	{"mostly identical length 10 B", ".1234567..", ",,1234567,", true},
-	{"mostly identical length 10 C", "..1234567.", ",1234567,,", true},
+	{"identical length 8", "12345679", "12345678", true},
+	{"identical length 9", "123456796", "123456796", true},
+	{"identical length 10", "1234567962", "1234567962", true},
+	{"different length 1", "8", "0", false},
+	{"different length 2", "88", "00", false},
+	{"different length 3", "888", "000", false},
+	{"different length 4", "8888", "0000", false},
+	{"different length 5", "88888", "00000", false},
+	{"different length 6", "888888", "000000", false},
+	{"different length 7", "8888888", "0000000", false},
+	{"different length 8", "88888888", "00000000", false},
+	{"different length 9", "888888888", "000000000", false},
+	{"different length 10", "8888888888", "0000000000", false},
+	{"mostly identical length 7", "1234568", "0123456", false},
+	{"mostly identical length 8", "12345678", "01234567", true},
+	{"mostly identical length 9 A", "123456788", "001234567", true},
+	{"mostly identical length 9 B", "812345678", "012345670", true},
+	{"mostly identical length 10 A", "1234567888", "0001234567", true},
+	{"mostly identical length 10 B", "8123456788", "0012345670", true},
+	{"mostly identical length 10 C", "8812345678", "0123456700", true},
 };
 
 
-TEST_F(CommonSubstringTests, MatchTest)
+template <typename T>
+class CommonSubstringTests : public ::testing::Test {};
+
+typedef ::testing::Types<
+	strings::common_substr_fast<
+		CommonSubstringTestParams::MaxSize,
+		CommonSubstringTestParams::SubstringSizeToTest
+	>,
+	strings::common_substr_hasharray<
+		CommonSubstringTestParams::MaxSize,
+		CommonSubstringTestParams::SubstringSizeToTest
+	>,
+	strings::common_substr_bitparallel_wrapper<
+		unsigned long long, '0', '9',
+		CommonSubstringTestParams::MaxSize,
+		CommonSubstringTestParams::SubstringSizeToTest
+	>
+> CommonSubstringTypes;
+TYPED_TEST_CASE(CommonSubstringTests, CommonSubstringTypes);
+
+TYPED_TEST(CommonSubstringTests, MatchTest)
 {
-	for (const auto& entry : KnownEntries)
+	for (const auto& entry : CommonSubstringTestParams::KnownEntries)
 	{
 		size_t l1 = strlen(entry.str1);
 		size_t l2 = strlen(entry.str2);
 		EXPECT_EQ(entry.has_common_substring,
-			test_type::match(entry.str1, l1, entry.str2, l2))
+			TypeParam::match(entry.str1, l1, entry.str2, l2))
 			<< "match test failed on <" << entry.description << ">";
 	}
 }
 
-TEST_F(CommonSubstringTests, MatchCommutativityTest)
+TYPED_TEST(CommonSubstringTests, MatchCommutativityTest)
 {
-	for (const auto& entry : KnownEntries)
+	for (const auto& entry : CommonSubstringTestParams::KnownEntries)
 	{
 		size_t l1 = strlen(entry.str1);
 		size_t l2 = strlen(entry.str2);
 		EXPECT_EQ(
-			test_type::match(entry.str1, l1, entry.str2, l2),
-			test_type::match(entry.str2, l2, entry.str1, l1))
+			TypeParam::match(entry.str1, l1, entry.str2, l2),
+			TypeParam::match(entry.str2, l2, entry.str1, l1))
 			<< "match commutativity test failed on <" << entry.description << ">";
 	}
 }
